30750ad5e8fb621b7cc9c7ca5e1480417091c989
soumi7 <soumibardhan10@gmail.com>
2020-12-06 18:20:43 +0530

    ?

--- test_api_endpoints.py
+++ test_api_endpoints.py
@@ -1,18 +1,19 @@
 import requests 
   
 # api-endpoint 
-URL = "http://localhost:80/predict_api"
+API_ENDPOINT = "http://localhost:80/predict_api"
   
 # location given here 
 location = "case study"
   
 # defining a params dict for the parameters to be sent to the API 
-PARAMS = {'sentences':location} 
+data = {'sentences':["case study"]} 
   
 # sending get request and saving the response as response object 
-r = requests.get(url = URL, params = PARAMS) 
+r = requests.post(url = API_ENDPOINT, data = data) 
   
 # extracting data in json format 
-#data = r.json() 
+data = r.json() 
 
-print(r.json() )
+print(data)
+print(data['predictions'])
