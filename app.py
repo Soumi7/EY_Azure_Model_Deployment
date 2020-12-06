eb2eed44e47acc4d7cd1830f50dc1dcb74887a28
soumi7 <soumibardhan10@gmail.com>
2020-12-06 21:59:48 +0530

    apps

--- app.py
+++ app.py
@@ -4,24 +4,35 @@ import os
 import numpy as np
 import json
 app = Flask(__name__)
+#from tensorflow import keras
+#import bert
+
+model = tf.keras.models.load_model('saved_model/ey_model')
+
 
 
 @app.route('/')
 def home():
     return 'Hello World'
 
+@app.route('/predict_api', methods=["POST"])
+def list_post():
+    json_body = request.get_json()
+    print(json_body[0])
+    print(json_body[1])
 
-    
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    json_body = request.get_json(force=True)
-    print(json_body)
-    print((request.query_string).decode("utf-8"))
-    predictions = [1,0,0,1]
+    predictions = model.predict(json_body).argmax(axis=-1)
+    #predictions = 2 * json_body[0]    
+    # * this is show query params needs to parsed manually
+    #print((request.query_string).decode("utf-8"))
 
-    return jsonify({'predictions': predictions})
+
+    return {
+        "predictions": predictions
+    }
 
 
 if __name__ == '__main__':
     #app.run(debug=True)
     app.run(host='0.0.0.0',port=80)
+
--- test_api_endpoints.py
+++ test_api_endpoints.py
@@ -1,19 +1,19 @@
 import requests 
 import json
 # api-endpoint 
-API_ENDPOINT = "http://0.0.0.0:5000/lists"
+API_ENDPOINT = "http://0.0.0.0:80/predict_api"
   
   
 # defining a params dict for the parameters to be sent to the API 
-data = {'sentence_tokens':[[0, 1, 1, 0],[2, 3 , 4, 1]]} 
-data_json= json.dumps(data)
+data = [[0, 1, 1, 0],[2, 3 , 4, 1]]
+#data_json= json.dumps(data)
 #print(data_json)
   
 # sending get request and saving the response as response object 
-r = requests.post(url = API_ENDPOINT, data = data_json) 
+r = requests.post(url = API_ENDPOINT, json = data) 
   
 # extracting data in json format 
 data = r.json() 
 
 print(data)
-print(data['resp'])
+print(data['predictions'])
